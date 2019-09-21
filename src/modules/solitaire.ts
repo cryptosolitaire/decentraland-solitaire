0b700528a9a43d2606e7f6ef36ede6acc33da794
Aldrick Bonaobra <aldrickbonaobra@gmail.com>
2019-09-21 15:37:41 +0800

    Update spacingZ

--- src/modules/solitaire.ts
+++ src/modules/solitaire.ts
@@ -40,7 +40,7 @@ export function SolitaireGame() : void{
     pilebasetop2 = dealbasetop;
     pilebaseleft2 = cardbaseleft["cardplaybase2"] ;
     spacingY = 0.001;
-    spacingZ = 0.1;
+    spacingZ = 0.004;
 
 
     // Define all cards
