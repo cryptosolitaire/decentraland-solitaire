26ed6adbc976a5fa307b942a05436ff2cbf9a878
Aldrick Bonaobra <aldrickbonaobra@gmail.com>
2019-09-24 15:33:21 +0800

    Add shuffle function to new game functions group

--- src/modules/solitaire.ts
+++ src/modules/solitaire.ts
@@ -147,22 +147,6 @@ export function SolitaireGame() : void{
         }
     }
 
-    function newGame(){
-        shuffle(allEntityCards);
-        resetAllCards();
-        refreshPositionAll();
-        refreshRotationAll();
-    }
-    /// ------------------
-
-    // Define all cards
-    var allCards = ["ac","ad","ah","as","2c","2d","2h","2s","3c","3d","3h","3s","4c","4d","4h","4s","5c","5d","5h","6s","6c","6d","6h","6s","7c","7d","7h","7s","8c","8d","8h","8s","9c","9d","9h","9s","10c","10d","10h","10s","jc","jd","jh","js","qc","qd","qh","qs","kc","kd","kh","ks"];
-
-    // Render all cards
-    var allEntityCards = [];
-
-
-
     // To shuffle array
     function shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
@@ -182,6 +166,20 @@ export function SolitaireGame() : void{
 
         return array;
     }
+
+    function newGame(){
+        shuffle(allEntityCards);
+        resetAllCards();
+        refreshPositionAll();
+        refreshRotationAll();
+    }
+    /// ------------------
+
+    // Define all cards
+    var allCards = ["ac","ad","ah","as","2c","2d","2h","2s","3c","3d","3h","3s","4c","4d","4h","4s","5c","5d","5h","6s","6c","6d","6h","6s","7c","7d","7h","7s","8c","8d","8h","8s","9c","9d","9h","9s","10c","10d","10h","10s","jc","jd","jh","js","qc","qd","qh","qs","kc","kd","kh","ks"];
+
+    // Render all cards
+    var allEntityCards = [];
 
     function addOnClick(index){
         if(allEntityCards[index]["name"] == "ac"){
