0dd693f9a7b73adf86afd1528c333562b352c052
Aldrick Bonaobra <aldrickbonaobra@gmail.com>
2019-09-24 15:35:21 +0800

    Update initialLoad function

--- src/modules/solitaire.ts
+++ src/modules/solitaire.ts
@@ -191,8 +191,6 @@ export function SolitaireGame() : void{
         prepareAllCards();
         loadAllCards();
         loadCardBases();
-        refreshPositionAll();
-        refreshRotationAll();
     }
     /// ----------------------
 
