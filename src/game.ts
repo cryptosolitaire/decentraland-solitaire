089829bceb76f0317138cc9840c4575d872e1502
Aldrick Bonaobra <aldrickbonaobra@gmail.com>
2019-09-26 13:20:34 +0800

    Add platform model and world.ts

--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,9 @@
 import { CreateStandee } from "./modules/standee";
 import { SolitaireGame } from "./modules/solitaire";
 import { SolitaireGameVertical } from "./modules/solitaire-vertical";
+import { World } from "./modules/world";
 
 //CreateStandee();
 //SolitaireGame();
 SolitaireGameVertical();
+World();
